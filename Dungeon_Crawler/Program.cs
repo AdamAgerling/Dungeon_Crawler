9fafa594f7b11d018c0c4b112b600c2c43cf290e
AdamAgerling <alltidvakenhehe@gmail.com>
2024-09-26 12:26:41 +0200

    Add project files.

---
+++ Dungeon_Crawler/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+string getCurrentDirectory = Directory.GetCurrentDirectory();
+
+string level1 = Path.Combine(getCurrentDirectory, "Levels", "Level1.txt");
+
+if (File.Exists(level1))
+    {
+    string Level1Content = File.ReadAllText(level1);
+
+    Console.WriteLine(Level1Content);
+    }
+else
+    {
+    Console.WriteLine("The file could not be found");
+    }
